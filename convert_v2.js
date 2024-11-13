a2b0d46c8a9918654c51af83c60f75c03003ce1e
ongyichong <yichong96@gmail.com>
2024-11-13 23:45:49 +0800

    fix: async csv parsing bug

--- convert_v2.js
+++ convert_v2.js
@@ -1,15 +1,25 @@
-const { IPA_PRONUNCIATIONS } = require('./csv_to_json');
 const fs = require('fs');
 const path = require('path');
+const csv = require('fast-csv');
+
+function parseCSVtoJSON(filePath) {
+    return new Promise((resolve, reject) => {
+        const jsonObject = {};
+        fs.createReadStream(path.resolve(filePath))
+            .pipe(csv.parse({ ignoreEmpty: true }))
+            .on('error', (error) => reject(error))
+            .on('data', (row) => jsonObject[row[0].trim()] = row[1].trim())
+            .on('end', () => resolve(jsonObject));
+    })
+}
 
 
 // Define a function to replace words with their IPA pronunciation
-function replaceWordsWithIPA(text) {
+function replaceWordsWithIPA(text, db) {
     // Define replacements
-
     // Replace each word with its IPA pronunciation
-    Object.keys(IPA_PRONUNCIATIONS).forEach(function(word) {
-        var ipa = IPA_PRONUNCIATIONS[word];
+    Object.keys(db).forEach(function(word) {
+        var ipa = db[word];
         var wordRegex = new RegExp(`(?<!<phoneme[^>]*\\bph="[^"]*">\\s*)\\b${word}\\b(?!<\\/phoneme>)`, 'g');
         var existingWordRegex = new RegExp(`<phoneme[^>]*>.*?${word}.*?<\/phoneme>`, 'g');
         // console.log(existingWordRegex)
@@ -27,18 +37,21 @@ function replaceWordsWithIPA(text) {
 // Define a regular expression to match content within <prosody> tag
 var prosodyRegex = /(<prosody[^>]*>)(.*?)(<\/prosody>)/s;
 
-const main = () => {
+const main = async () => {
     const [xmlInputPath, xmlOutputPath] = process.argv.slice(2);
     const xmlData = fs.readFileSync(path.resolve(xmlInputPath), 'utf8');
 
-    // backup original file from server
+    const filePath = path.join(__dirname, 'KJV Bible Reading Plan - Pronunciation Fix.csv');
+    const IPA_PRONUNCIATIONS = await parseCSVtoJSON(filePath);    // backup original file from server
+    
     fs.writeFileSync(path.resolve(xmlInputPath.split('.')[0] + 'backup.xml'), xmlData, 'utf8');
 
     // Replace occurrences within the <prosody> tag
     var updatedXmlString = xmlData.replace(prosodyRegex, function(match, startTag, content, endTag) {
         // Replace words with their IPA pronunciations
-        return startTag + replaceWordsWithIPA(content) + endTag;
-    });    
+        return startTag + replaceWordsWithIPA(content, IPA_PRONUNCIATIONS) + endTag;
+    });
+    console.log(updatedXmlString);
 
     fs.writeFileSync(path.resolve(xmlOutputPath), updatedXmlString, 'utf8');
 }
